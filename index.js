6606f723b2e3863068be0b34cabca93b72b785b6
Borislav Kokeza <bkokeza@planetsoft.ba>
2023-03-08 11:21:08 +0100

    .

---
+++ index.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const cors = require('cors');
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+
+const app = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+
+app.db = router.db;
+const corsOptions = {
+  origin: 'http://167.86.109.4:8022',
+  credentials: true,
+  methods: 'GET, POST, PUT, PATCH, DELETE',
+};
+
+app.use(cors(corsOptions));
+
+app.use(jsonServer.bodyParser);
+app.use(middlewares);
+
+app.use(auth);
+app.use(router);
+
+const PORT = 8877;
+
+app.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
